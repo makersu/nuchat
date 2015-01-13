bdf0f4771fafb7829b56fad6a1aa8b7a08333269
Maker Su <yuminsutw@yahoo.com.tw>
2015-01-13 16:34:50 +0800

    create socket.io module and add model message, mongodb connector

--- server/boot/sio.js
+++ server/boot/sio.js
@@ -1,10 +1,18 @@
 module.exports = function(app) {
   app.on('started', function() {
-
+  	
 	  app.sio.on('connection', function(socket){
 	  	console.log('a user connected');
 	  	socket.on('chat message', function(msg){
 	    	console.log('message: ' + msg);
+
+	    	app.models.Message.create({text:msg}, function(err, obj){
+	    		if(err){
+	    			console.log('err='+err);
+	    		}
+	    		console.log('created='+JSON.stringify(obj));
+	    	})
+
 	    	app.sio.emit('chat message', msg);
 	  	});
 	  	socket.on('disconnect', function(){
