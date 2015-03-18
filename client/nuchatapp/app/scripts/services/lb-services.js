5120b9aeb08da50b33046eafb4ae1920562f5557
Maker Su <yuminsutw@yahoo.com.tw>
2015-03-18 18:20:22 +0800

    update for production

--- client/nuchatapp/app/scripts/services/lb-services.js
+++ client/nuchatapp/app/scripts/services/lb-services.js
@@ -1,10 +1,7 @@
 (function(window, angular, undefined) {'use strict';
 
 var authHeader = 'authorization';
-var urlBase = 'http://54.92.67.230:3333/api';
-//var urlBase = 'http://10.0.0.106:3333/api';
-// For Android Development using genymotion.
-//var urlBase = 'http://192.168.56.1:3333/api';
+var urlBase = 'http://140.123.4.17:3001/api';//
 
 /**
  * @ngdoc overview
