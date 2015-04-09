342aae2097f4468c14288bfb525f7a7d06eef417
Maker Su <yuminsutw@yahoo.com.tw>
2015-04-09 22:58:19 +0800

    use operation hooks instead of deprecated Model hooks

--- common/models/message.js
+++ common/models/message.js
@@ -1,9 +1,10 @@
 module.exports = function(message) {
-	message.beforeCreate = function(next, modelInstance) {
-		modelInstance.created = new Date();
-		console.log(modelInstance)
-	  //your logic goes here
+
+	message.observe('before save', function createdTimestamp(ctx, next) {
+	  if (ctx.instance) {
+	    ctx.instance.created = new Date();
+	  } 
 	  next();
-	};
+	});
 
 };
