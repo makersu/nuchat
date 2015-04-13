3a79430c605aba8efc371df8ccb672f633a5cb93
Maker Su <yuminsutw@yahoo.com.tw>
2015-04-13 11:38:22 +0800

    add for create avatar letter after register

--- client/nuchatapp/app/scripts/services/accountService.js
+++ client/nuchatapp/app/scripts/services/accountService.js
@@ -1,4 +1,4 @@
-function AccountService(User, ENV) {
+function AccountService(User, ENV, LBSocket) {
 
   var avatarUrl;
 
@@ -6,21 +6,33 @@ function AccountService(User, ENV) {
     return avatarUrl;
   }
 
+  //Refactoring if no profile
   function setAvatarUrl(profile){
     console.log('setAvatarUrl')
     var avatarThumbnail;
+    var avatarLetter;
     if(profile){
       avatarThumbnail=profile.avatarThumbnail
     }
     else{
       avatarThumbnail=User.getCachedCurrent().avatarThumbnail
+      avatarLetter=User.getCachedCurrent().avatarLetter
     }
 
     if(avatarThumbnail){
       avatarUrl=ENV.GRIDFS_BASE_URL+avatarThumbnail
     }
+    else if(avatarLetter){
+      avatarUrl=ENV.GRIDFS_BASE_URL+avatarLetter
+    }
     else{
       avatarUrl=undefined;
+      console.log('user:profile:letteravatar')
+      LBSocket.emit('user:profile:letteravatar', User.getCachedCurrent(),function(err,profile){
+        console.log(profile)
+        avatarLetter=profile.avatarLetter
+        avatarUrl=ENV.GRIDFS_BASE_URL+avatarLetter
+      });//
     }
     console.log(avatarUrl)
   }
