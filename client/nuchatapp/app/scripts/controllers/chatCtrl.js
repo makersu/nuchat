96469261af863ffc5ebcbb6fd36f3f77318814d6
James <heroms90@gmail.com>
2015-05-20 10:39:21 +0800

    Adjusted the filtering by member to adapt to the new structure of room.

--- client/nuchatapp/app/scripts/controllers/chatCtrl.js
+++ client/nuchatapp/app/scripts/controllers/chatCtrl.js
@@ -26,7 +26,7 @@ function ChatCtrl($scope, $rootScope, $document, $state, $stateParams, $animate,
   // Scope Public
 	$scope.currentUser = User.getCachedCurrent();
   $scope.currentUser.avatarThumbnail = ENV.GRIDFS_BASE_URL+$scope.currentUser.avatarThumbnail;
-  $scope.otherUsers = [];
+  $scope.joinerList = [];
   // console.log($scope.currentUser)//
   $scope.input = {};
   $scope.messageOptions = {
@@ -121,13 +121,16 @@ function ChatCtrl($scope, $rootScope, $document, $state, $stateParams, $animate,
   /* Getting the all the users joined this room except the currentUser
    */
   function getRoomUsers() {
-    $scope.otherUsers = [];
+    $scope.joinerList = [];
     // console.log($scope.room);//
-    if ( RoomService.isPrivate($scope.room) ) {
-      $scope.otherUsers.push($scope.friends[$scope.room.ownerId === $scope.currentUser.id ? $scope.room.friend : $scope.room.ownerId]);
-    } else {
-      // TODO: Getting user in the group.
-    }
+    angular.forEach($scope.room.joiners, function(joiner) {
+      $scope.friends[joiner] && $scope.joinerList.push($scope.friends[joiner]);
+    });
+    // if ( RoomService.isPrivate($scope.room) ) {
+    //   $scope.otherUsers.push($scope.friends[$scope.room.ownerId === $scope.currentUser.id ? $scope.room.friend : $scope.room.ownerId]);
+    // } else {
+    //   // TODO: Getting user in the group.
+    // }
   }
 
   // Scope Public
@@ -321,6 +324,12 @@ function ChatCtrl($scope, $rootScope, $document, $state, $stateParams, $animate,
       }
       group.items = $filter('filter')(group.originalItems, { ownerId: user.id });
       console.log(group.items);
+    });
+  };
+  $scope.getAllMessages = function() {
+    angular.forEach($scope.room.groupedMessages, function(group) {
+      group.items = group.originalItems;
+      // console.log(group.items);
     });
   };
 
